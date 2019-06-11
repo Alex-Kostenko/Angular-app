9aed9790a5f1dcc7dead8f16bf8bdaf17291372d
Alex <alex@Mac-mini-Alex.local>
2019-06-11 11:44:31 +0300

    include bootstrap

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,18 +6,18 @@ export interface Student {
   phoneNumber: string;
 }
 
-export interface StudentDetails {
-  [key: number]: Student; //Or string instead of number
-}
-
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.sass']
+  styleUrls: ['./test.component.sass', ]
 })
 
 
 export class TestComponent implements OnInit {
+
+  text = 'Hello Word';
+  text1 = 'текст1';
+
   constructor() { }
 
   ngOnInit() {
